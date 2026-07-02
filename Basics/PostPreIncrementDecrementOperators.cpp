f36178537fab68d13caceb2f641270e3a185c960
divyanshubose2408-a11y <divyanshubose2408@gmail.com>
2026-07-02 21:19:43 +0530

    Create PostPreIncrementDecrementOperators.cpp

---
+++ Basics/PostPreIncrementDecrementOperators.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // x++ ++x x-- --x
+    
+    int x = 10;
+    cout<<x<<endl;
+    --x; // x = x - 1
+    cout<<x<<endl;
+    
+    // int x = 10;
+    // cout<<x<<endl;
+    // ++x; // x = x + 1
+    // cout<<x<<endl;
+}
+#include <iostream>
+using namespace std;
+int main(){
+    // int x = 10;
+    // cout<<x--<<endl;
+    // cout<<x<<endl;
+    
+    int x = 10;
+    cout<<--x<<endl;
+    cout<<x<<endl;
+}
